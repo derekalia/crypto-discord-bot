ea90ea9796abf8090ae5b9c14cf624efe2dcd045
Mr. Benji <thebkrcanbefound@gmail.com>
2019-01-24 21:04:39 -0500

    Adds functionality to grab uploaded and linked media

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const keys = require('./config/keys');
 const Web3 = require('web3');
 let web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + keys.infura));
 
+const algoliasearch = require('algoliasearch');
+// const client = algoliasearch(keys.algoliaAppId, keys.algoliaAdminKey);
+// const postIndex = client.initIndex('Post010819');
+require('./models/Post');
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
+
 app.get('/', function(req, res) {
   res.send('hello world');
 });
@@ -89,30 +102,60 @@ client.on('message', async message => {
     id: message.guild.id,
   }
 
-  // console.log('guild ===', message.guild.name)
-
   //NOTE: For if a user uploads an image
   if ((message.attachments).array()[0]) {
-    const Attachment = (message.attachments).array();
-    const link = Attachment[0].url;
-    // console.log('HERE =====', link);
+
+    const attachment = (message.attachments).array();
+    const link = attachment[0].url;
+    const fetchedPost = await Post.findOne({
+      linkUUID: 'b7762018'
+    });
+    console.log('Post ----', fetchedPost)
+
+
+    const post = new Post({
+      title: "Legend Of the Five Rings - Update 4",
+      description: "Description thingy",
+      media: "5bd7abf602d5491325aca485/0eb339c0-dc19-11e8-9b93-93df198b4ee8.png",
+      author: "pancho",
+      authorAddress: "",
+      type: "request",
+      walletAddress: "",
+      linkTitle: "lesadfds_of_the_five_rings_-_update_4",
+      linkUUID: "b7862018",
+      free: true,
+      request: false,
+      tribeLink: "Fantasy_Flight_Games",
+      tribeName: "Fantasy Flight Games",
+      tribeId: "5bd7ca514f43101435fbe649",
+      selectedSubmission: null,
+      NSFW: false,
+      sticky: false,
+    });
+
+    //Add tribe to Algolia
+    // postIndex.addObjects(post);
+
+    // try {
+    //   let updatedPost = await post.save();
+    //   // await Tribe.update({ _id: tribeId }, { $push: { posts: updatedPost._id } });
+    //   console.log('IT POSTED :D ==', updatedPost);
+    //   // res.send(post);
+    // } catch (err) {
+    //   console.log('errored saving ====', { err });
+    //   // res.send(400, err);
+    // }
+    
   }
 
-  //NOTE: For if a user pastes a link to an image, give etc. Currently ONLY works for Imgur (easy to change to reddit etc.)
-  //https://i.imgur.com/XKPjzZz.gifv  https://imgur.com/gallery/eFHlaQG
-  // if (message.content.includes('imgur.com'||'reddit.com')) { //if it contains an invite link
-  //   var domainIndex = message.content.indexOf("imgur");
-  //   var theUrl = message.content.slice(domainIndex, domainIndex + 25);
-  //   console.log('the link posted was: ', theUrl)
-  // }
-
+  //NOTE: For if a user pastes a link with an embeded image
   if (message.embeds[0]) {
     const mediaLink = message.embeds[0].url;
     console.log('media ===', mediaLink)
     //save to DB
   }
 
-  //https://i.redd.it/vr994idtz5b21.jpg https://i.redd.it/1q90uhb5r5b21.jpg
+
 
 
   if (message.content === 'ping') {
